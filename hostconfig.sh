5ae4e182d3ddbede2700ed004bef58b46e075b39
Mike Gaffney <mike@uberu.com>
2013-07-31 21:31:56 -0700

    Add nodejs to squid proxy allow

--- hostconfig.sh
+++ hostconfig.sh
@@ -1,3 +1,4 @@
 sudo apt-get install squid-deb-proxy
+sudo cp 20-nodejs /etc/squid-deb-proxy/mirror-dstdomain.acl.d/20-nodejs
 HOST_IP=`ifconfig docker0 |grep inet|head -1|sed 's/\:/ /'|awk '{print $3}'`
 echo "Acquire::http::Proxy \"http://$HOST_IP:8000\";" > 30proxy
